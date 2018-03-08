fad8df805f34ebfdb37cd488c271cea8b349db0b
Mykhailo Kos <mishakos@hotmail.com>
2018-03-08 14:04:17 +0200

    finish actions inline

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,6 +1,8 @@
 import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+
 
 class CoursesPage extends React.Component {
     constructor(props, context) {
@@ -22,7 +24,7 @@ class CoursesPage extends React.Component {
     }
 
     onClickSave(event) {
-        this.props.dispatch(courseActions.createCourse(this.state.course));
+        this.props.actions.createCourse(this.state.course);
     }
 
     courseRow(course, index) {
@@ -43,8 +45,8 @@ class CoursesPage extends React.Component {
 }
 
 CoursesPage.propTypes ={
-    dispatch: PropTypes.func.isRequired,
-    courses: PropTypes.array.isRequired
+    courses: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
@@ -53,4 +55,10 @@ function mapStateToProps(state, ownProps) {
     };
 }
 
-export default connect(mapStateToProps)(CoursesPage);
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(courseActions, dispatch)
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
